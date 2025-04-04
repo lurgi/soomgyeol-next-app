ad020606419036e77007c22f752a53a25f370f74
Jeongwoo Park <wahoo9040@naver.com>
2025-04-04 14:40:18 +0900

    feat: Add Header component

---
+++ app/components/Header/index.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Image from "next/image";
+import { Search, Bell, ChevronLeft } from "lucide-react";
+import { Title } from "../font";
+
+type HeaderType = "main" | "detail";
+
+interface HeaderProps {
+  type?: HeaderType;
+  title?: string;
+  hasIcons?: boolean;
+}
+
+export default function Header({ type = "main", title = "숨결", hasIcons = true }: HeaderProps) {
+  return (
+    <header className="w-full py-[10px] px-[16px] fixed top-0 left-0 right-0  h-15 bg-background z-50">
+      <div className="container flex justify-between items-center">
+        {/* Left Section */}
+        <div className="flex items-center gap-3">
+          {type === "main" && <Image src="/logo.svg" alt="숨결 로고" width={29} height={40} className="mr-1" />}
+          {type === "detail" && (
+            <button aria-label="뒤로가기" className="ml-[-8px]">
+              <ChevronLeft width={40} height={40} />
+            </button>
+          )}
+
+          <Title.T2 weight="medium">{title}</Title.T2>
+        </div>
+
+        {/* 아이콘들 */}
+        <div className="flex items-center gap-3">
+          {hasIcons && (
+            <>
+              <button aria-label="검색">
+                <Search className="w-6 h-6" />
+              </button>
+              <button aria-label="알림">
+                <Bell className="w-6 h-6" />
+              </button>
+            </>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+}

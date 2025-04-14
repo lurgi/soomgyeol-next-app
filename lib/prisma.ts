bb6d499660357a15ffd5155d2694066ac2fa3398
Jeongwoo Park <wahoo9040@naver.com>
2025-04-14 20:30:44 +0900

    chore: setting prisma client and schema

---
+++ lib/prisma.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from "@prisma/client";
+
+// PrismaClient는 전역 싱글톤으로 사용하여 연결 수를 최소화
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
